9c0a6ec9015e04f6085628c0d476d4b15f4a0de2
osamagozlan <o.gozlan@emakina.nl>
2022-12-27 16:55:58 +0100

    fix reload onfilter page

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,28 +1,31 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   useSelectedVariable,
   useReleatedStories,
 } from "../../hooks/useReleatedStories";
-import { ICat, ITag } from "../../types/types";
 import NetworkGraph from "../NetworkGraph";
 
 const Filter = () => {
   const { slug } = useParams();
   const releatedStories = useReleatedStories(slug as string);
   const selectedVariable = useSelectedVariable(slug as string);
+
   const title =
     (selectedVariable?.tag as string) || (selectedVariable?.category as string);
   const definition = selectedVariable?.definition;
 
   return (
     <>
-      <h1>{title}</h1>
-      <p>{definition}</p>
-      <NetworkGraph
-        selectedVariable={selectedVariable}
-        relatedStories={releatedStories}
-      />
+      {selectedVariable?.category && releatedStories?.length > 0 && (
+        <>
+          <h1>{title}</h1>
+          <p>{definition}</p>
+          <NetworkGraph
+            selectedVariable={selectedVariable}
+            relatedStories={releatedStories}
+          />
+        </>
+      )}
     </>
   );
 };
