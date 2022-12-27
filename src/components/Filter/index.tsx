19a5c452abc16e45b2b4f35002b86d25d06c3904
osamagozlan <o.gozlan@emakina.nl>
2022-12-27 16:57:54 +0100

    Revert "fix reload onfilter page"
    This reverts commit 9c0a6ec9015e04f6085628c0d476d4b15f4a0de2.

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,31 +1,28 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   useSelectedVariable,
   useReleatedStories,
 } from "../../hooks/useReleatedStories";
+import { ICat, ITag } from "../../types/types";
 import NetworkGraph from "../NetworkGraph";
 
 const Filter = () => {
   const { slug } = useParams();
   const releatedStories = useReleatedStories(slug as string);
   const selectedVariable = useSelectedVariable(slug as string);
-
   const title =
     (selectedVariable?.tag as string) || (selectedVariable?.category as string);
   const definition = selectedVariable?.definition;
 
   return (
     <>
-      {selectedVariable?.category && releatedStories?.length > 0 && (
-        <>
-          <h1>{title}</h1>
-          <p>{definition}</p>
-          <NetworkGraph
-            selectedVariable={selectedVariable}
-            relatedStories={releatedStories}
-          />
-        </>
-      )}
+      <h1>{title}</h1>
+      <p>{definition}</p>
+      <NetworkGraph
+        selectedVariable={selectedVariable}
+        relatedStories={releatedStories}
+      />
     </>
   );
 };
